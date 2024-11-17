9b6056aa5fffd16f3d0f42735f702bb6a09e278f
Florian Thiel <flth@flth.de>
2024-11-17 20:05:47 +0100

    initial

---
+++ strings.lua
@@ -0,0 +1,24 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "Electric Mobility",
+		["mod_desc"] = "",
+		["name_ladepark"] = "EnBW Charging Park",
+		["desc_ladepark"] = "Asset for electric car.",
+		["extensions"] = "Extenstions",
+		["reflection"] = "Reflection",
+		["roof"] = "Glass roof",
+		["charger"] = "2. Charging column",
+	},
+	de = {
+		["mod_name"] = "Elektromobilität",
+		["mod_desc"] = "",
+		["name_ladepark"] = "EnBW Ladepark",
+		["desc_ladepark"] = "Ein Asset für E-Autos.",
+		["extensions"] = "Erweiterungen",
+		["reflection"] = "Spiegelung",
+		["roof"] = "Glasdach",
+		["charger"] = "2. Ladesäule",
+	},
+}
+end
